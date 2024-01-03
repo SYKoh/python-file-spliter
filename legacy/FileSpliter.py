404e9d2bc0dcf8867b5a94368f22f03594935070
SYKoh <313056749@qq.com>
2024-01-03 13:46:22 +0800

    commit legacy change

--- legacy/FileSpliter.py
+++ legacy/FileSpliter.py
@@ -1,16 +1,13 @@
 import tkinter as tk
 from tkinter import filedialog
-import os
 
 def split_file(input_file, output_file_prefix, chunk_size):
-    _, input_file_extension = os.path.splitext(input_file)
-
-    with open(input_file, 'rt', encoding='utf-8') as f:
+    with open(input_file, 'r') as f:
         lines = f.readlines()
 
     for i in range(0, len(lines), chunk_size):
-        output_file = f"{output_file_prefix} {i + 1}-{i + chunk_size}{input_file_extension}"
-        with open(output_file, 'wt', encoding='utf-8') as f:
+        output_file = f"{output_file_prefix} {i + 1}-{i + chunk_size}.txt"
+        with open(output_file, 'w') as f:
             f.writelines(lines[i:i + chunk_size])
 
 def select_file():
--- legacy/README.md
+++
@@ -1,5 +0,0 @@
-# Python File Spliter
-
-made by ChatGPT
-
-[prompt link](https://chat.openai.com/share/cb9fc9d3-1ddc-4bc9-9689-6a0cb7b42cfa)
